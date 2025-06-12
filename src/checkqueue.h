ad098a8911fcec7b1b95906d81ed7ca37d742b94
Hao Xu <hao.xu@linux.dev>
2025-06-12 16:24:00 +0800

    checkqueue: local queue based transaction verification worker pool
    Signed-off-by: Hao Xu <hao.xu@linux.dev>

--- src/checkqueue.h
+++ src/checkqueue.h
@@ -15,6 +15,155 @@
 #include <optional>
 #include <vector>
 
+template <typename T, typename R = std::remove_cvref_t<decltype(std::declval<T>()().value())>>
+class CCheckQueue;
+
+template <typename T, typename R = std::remove_cvref_t<decltype(std::declval<T>()().value())>>
+class CWorker {
+private:
+    CCheckQueue<T, R> *cqueue;
+    //! Mutex to protect the inner state
+    Mutex m_mutex;
+
+    int id;
+    //! Worker threads block on this when out of work
+    std::condition_variable m_worker_cv;
+
+    //! The queue of elements to be processed.
+    //! As the order of booleans doesn't matter, it is used as a LIFO (stack)
+    std::vector<T> queue GUARDED_BY(m_mutex);
+    bool m_pool_destroy{false} GUARDED_BY(m_mutex);
+    bool m_master_on{false} GUARDED_BY(m_mutex);
+    unsigned int done{0};
+
+    /**
+     * the result turned out, finish verification
+     * use atomic rather than protect in m_mutex to avoid ABBA deadlock
+     */
+    std::atomic<bool> m_finish{false};
+
+    std::thread m_worker_thread;
+
+    //! The maximum number of elements to be processed in one batch
+    const unsigned int nBatchSize;
+
+
+    /** Internal function that does bulk of the verification work. */
+    void Worker() EXCLUSIVE_LOCKS_REQUIRED(!m_mutex)
+    {
+        std::vector<T> vChecks;
+        vChecks.reserve(nBatchSize*10);
+        unsigned int nNow = 0;
+        std::optional<R> local_result;
+        bool do_work;
+
+        do {
+            {
+                WAIT_LOCK(m_mutex, lock);
+                if (nNow) {
+                    done += nNow;
+                    if (local_result.has_value()) {
+                        LOCK(cqueue->m_result_lock);
+                        if (!cqueue->m_result.has_value()) {
+                            cqueue->SetFinish();
+                            std::swap(local_result, cqueue->m_result);
+                        }
+                    }
+                }
+
+
+                if (queue.empty() && m_master_on) {
+                    cqueue->IncrementDone(done);
+                    done = 0;
+                }
+                // first do the clean-up of the previous loop run (allowing us to do it in the same critsect)
+                // logically, the do loop starts here
+                while (queue.empty() && !m_pool_destroy) {
+                    m_worker_cv.wait(lock); // wait
+                }
+
+                if (m_pool_destroy) {
+                    // return value does not matter, because m_pool_destroy is only set in the destructor.
+                    return;
+                }
+
+                // Check whether we need to do work at all
+                do_work = !m_finish;
+                nNow = queue.size();
+                if (do_work) {
+                    // Decide how many work units to process now.
+                    // * Do not try to do everything at once, but aim for increasingly smaller batches so
+                    //   all workers finish approximately simultaneously.
+                    // * Try to account for idle jobs which will instantly start helping.
+                    // * Don't do batches smaller than 1 (duh), or larger than nBatchSize.
+                    vChecks.assign(std::make_move_iterator(queue.begin()), std::make_move_iterator(queue.end()));
+                }
+                queue.clear();
+            }
+            // execute work
+            if (do_work) {
+                for (T& check : vChecks) {
+                    local_result = check();
+                    if (local_result.has_value()) break;
+                }
+                vChecks.clear();
+            }
+        } while (true);
+    }
+
+public:
+//    CWorker(CWorker&&) = default;
+//    CWorker& operator=(CWorker&&) = default;
+    CWorker(int worker_index, unsigned int batch_size, CCheckQueue<T, R> *cq): cqueue(cq), nBatchSize(batch_size)
+    {
+        id = worker_index;
+        m_worker_thread = std::thread([this, worker_index]() {
+            util::ThreadRename(strprintf("scriptch.%i", worker_index));
+            Worker();
+        });
+    }
+
+    void Add(std::vector<T>&& vChecks) EXCLUSIVE_LOCKS_REQUIRED(!m_mutex) {
+        LOCK(m_mutex);
+        queue.insert(queue.end(), std::make_move_iterator(vChecks.begin()), std::make_move_iterator(vChecks.end()));
+    }
+
+    void Notify() {
+        m_worker_cv.notify_one();
+    }
+
+    void Join() {
+        m_worker_thread.join();
+    }
+
+    void MarkFinish() {
+        m_finish = true;
+    }
+
+    void CleanFinish() {
+        m_finish = false;
+    }
+
+    // m_mutex required
+    void MarkPoolDestroy() {
+        WITH_LOCK(m_mutex, m_pool_destroy = true);
+    }
+
+    // m_mutex required
+    void MarkMasterOn() {
+        {
+            LOCK(m_mutex);
+            m_master_on = true;
+        }
+        Notify();
+    }
+
+    void MarkMasterOff() {
+        LOCK(m_mutex);
+        m_master_on = false;
+    }
+};
+
 /**
  * Queue for verifications that have to be performed.
   * The verifications are represented by a type T, which must provide an
@@ -29,136 +178,38 @@
   * as an N'th worker, until all jobs are done.
   *
   */
-template <typename T, typename R = std::remove_cvref_t<decltype(std::declval<T>()().value())>>
+template <typename T, typename R>
 class CCheckQueue
 {
 private:
-    //! Mutex to protect the inner state
     Mutex m_mutex;
-
-    //! Worker threads block on this when out of work
-    std::condition_variable m_worker_cv;
-
+    unsigned int nTodo{0};
     //! Master thread blocks on this when out of work
     std::condition_variable m_master_cv;
 
-    //! The queue of elements to be processed.
-    //! As the order of booleans doesn't matter, it is used as a LIFO (stack)
-    std::vector<T> queue GUARDED_BY(m_mutex);
-
-    //! The number of workers (including the master) that are idle.
-    int nIdle GUARDED_BY(m_mutex){0};
-
-    //! The total number of workers (including the master).
-    int nTotal GUARDED_BY(m_mutex){0};
-
-    //! The temporary evaluation result.
-    std::optional<R> m_result GUARDED_BY(m_mutex);
-
     /**
-     * Number of verifications that haven't completed yet.
-     * This includes elements that are no longer queued, but still in the
-     * worker's own batches.
+     * Point to the worker index in m_workers, no need to lock it since there is
+     * only one producer
      */
-    unsigned int nTodo GUARDED_BY(m_mutex){0};
+    unsigned int next_worker{0};
 
-    //! The maximum number of elements to be processed in one batch
-    const unsigned int nBatchSize;
-
-    std::vector<std::thread> m_worker_threads;
-    bool m_request_stop GUARDED_BY(m_mutex){false};
-
-    /** Internal function that does bulk of the verification work. If fMaster, return the final result. */
-    std::optional<R> Loop(bool fMaster) EXCLUSIVE_LOCKS_REQUIRED(!m_mutex)
-    {
-        std::condition_variable& cond = fMaster ? m_master_cv : m_worker_cv;
-        std::vector<T> vChecks;
-        vChecks.reserve(nBatchSize);
-        unsigned int nNow = 0;
-        std::optional<R> local_result;
-        bool do_work;
-
-        if (fMaster) nTotal++;
-
-        do {
-            {
-                WAIT_LOCK(m_mutex, lock);
-                // first do the clean-up of the previous loop run (allowing us to do it in the same critsect)
-                if (nNow) {
-                    if (local_result.has_value() && !m_result.has_value()) {
-                        std::swap(local_result, m_result);
-                    }
-                    nTodo -= nNow;
-                    if (nTodo == 0 && !fMaster) {
-                        // We processed the last element; inform the master it can exit and return the result
-                        m_master_cv.notify_one();
-                    }
-                }
-                // logically, the do loop starts here
-                while (queue.empty() && !m_request_stop) {
-                    if (fMaster && nTodo == 0) {
-                        nTotal--;
-                        std::optional<R> to_return = std::move(m_result);
-                        // reset the status for new work later
-                        m_result = std::nullopt;
-                        // return the current status
-                        return to_return;
-                    }
-                    nIdle++;
-                    cond.wait(lock); // wait
-                    nIdle--;
-                }
-                if (m_request_stop) {
-                    // return value does not matter, because m_request_stop is only set in the destructor.
-                    return std::nullopt;
-                }
-
-                // Check whether we need to do work at all
-                do_work = !m_result.has_value();
-                if (!do_work) {
-                    nNow = queue.size();
-                    queue.clear();
-                } else {
-                    // Decide how many work units to process now.
-                    // * Do not try to do everything at once, but aim for increasingly smaller batches so
-                    //   all workers finish approximately simultaneously.
-                    // * Try to account for idle jobs which will instantly start helping.
-                    // * Don't do batches smaller than 1 (duh), or larger than nBatchSize.
-                    nNow = std::max(1U, std::min(nBatchSize, (unsigned int)queue.size() / (nTotal + nIdle + 1)));
-                    auto start_it = queue.end() - nNow;
-                    vChecks.assign(std::make_move_iterator(start_it), std::make_move_iterator(queue.end()));
-                    queue.erase(start_it, queue.end());
-                }
-            }
-            // execute work
-            if (do_work) {
-                for (T& check : vChecks) {
-                    local_result = check();
-                    if (local_result.has_value()) break;
-                }
-                vChecks.clear();
-            }
-        } while (true);
-    }
+    std::vector<std::unique_ptr<CWorker<T, R>>> m_workers;
 
 public:
+    Mutex m_result_lock;
+    //! The temporary evaluation result.
+    std::optional<R> m_result;
+
     //! Mutex to ensure only one concurrent CCheckQueueControl
     Mutex m_control_mutex;
 
     //! Create a new check queue
-    explicit CCheckQueue(unsigned int batch_size, int worker_threads_num)
-        : nBatchSize(batch_size)
-    {
+    explicit CCheckQueue(unsigned int batch_size, int worker_threads_num) {
         LogInfo("Script verification uses %d additional threads", worker_threads_num);
-        m_worker_threads.reserve(worker_threads_num);
+        m_workers.reserve(worker_threads_num);
         for (int n = 0; n < worker_threads_num; ++n) {
-            m_worker_threads.emplace_back([this, n]() {
-                util::ThreadRename(strprintf("scriptch.%i", n));
-                Loop(false /* worker thread */);
-            });
+            m_workers.emplace_back(std::make_unique<CWorker<T, R>>(n, batch_size, this));
         }
-
-        nTotal = worker_threads_num;
     }
 
     // Since this class manages its own resources, which is a thread
@@ -170,41 +221,73 @@ public:
 
     //! Join the execution until completion. If at least one evaluation wasn't successful, return
     //! its error.
-    std::optional<R> Complete() EXCLUSIVE_LOCKS_REQUIRED(!m_mutex)
-    {
-        return Loop(true /* master thread */);
+    std::optional<R> Complete() {
+        for (auto& w : m_workers)
+            w->MarkMasterOn();
+
+        while (true) {
+            WAIT_LOCK(m_mutex, lock);
+            if (!nTodo) {
+                for (auto& w : m_workers) {
+                    w->CleanFinish();
+                    w->MarkMasterOff();
+                }
+
+                LOCK(m_result_lock);
+                std::optional<R> to_return = std::move(m_result);
+                // reset the status for new work later
+                m_result = std::nullopt;
+                // return the current status
+                return to_return;
+            }
+
+            m_master_cv.wait(lock);
+        }
     }
 
     //! Add a batch of checks to the queue
-    void Add(std::vector<T>&& vChecks) EXCLUSIVE_LOCKS_REQUIRED(!m_mutex)
+    // no support for concurrent Add for now, since no lock for m_workers
+    void Add(std::vector<T>&& vChecks)
     {
         if (vChecks.empty()) {
             return;
         }
 
-        {
-            LOCK(m_mutex);
-            queue.insert(queue.end(), std::make_move_iterator(vChecks.begin()), std::make_move_iterator(vChecks.end()));
-            nTodo += vChecks.size();
-        }
-
-        if (vChecks.size() == 1) {
-            m_worker_cv.notify_one();
-        } else {
-            m_worker_cv.notify_all();
-        }
+        WITH_LOCK(m_mutex, nTodo += vChecks.size());
+        auto& worker = m_workers[next_worker];
+        next_worker = (next_worker + 1) % m_workers.size();
+        worker->Add(std::move(vChecks));
+        worker->Notify();
     }
 
     ~CCheckQueue()
     {
-        WITH_LOCK(m_mutex, m_request_stop = true);
-        m_worker_cv.notify_all();
-        for (std::thread& t : m_worker_threads) {
-            t.join();
+        for (auto& w : m_workers) {
+            w->MarkPoolDestroy();
+            w->Notify();
+        }
+
+        for (auto& w : m_workers) {
+            w->Join();
         }
     }
 
-    bool HasThreads() const { return !m_worker_threads.empty(); }
+    bool HasThreads() const { return !m_workers.empty(); }
+    void SetFinish() {
+        for (auto &w : m_workers) {
+            w->MarkFinish();
+        }
+    }
+
+    void Notify() {
+        m_master_cv.notify_one();
+    }
+
+    void IncrementDone(unsigned int done) {
+        LOCK(m_mutex);
+        nTodo -= done;
+        if (!nTodo) Notify();
+    }
 };
 
 /**
